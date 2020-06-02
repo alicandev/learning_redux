59a71f07aeaa5a08972b90f3415f8904c81f2537
= <62238518+demirtas-dev@users.noreply.github.com>
2020-06-02 09:12:38 +0100

    Written my first reducer and tested it. It takes in a state and an action, then, if the action type is SET_GOAL, we set the state to the action's payload. Which in my case was a hardcoded 15.

---
+++ src/store/reducers.js
@@ -0,0 +1,9 @@
+﻿import C from '../constants';
+
+export const goal = (state = 10, action) => {
+    if (action.type === C.SET_GOAL) {
+        return parseInt(action.payload)
+    } else { 
+        return state 
+    }
+}
