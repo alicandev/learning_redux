66f152e21cda2e4861869a1cb1da1ef92b9af89b
= <62238518+demirtas-dev@users.noreply.github.com>
2020-06-07 15:37:00 +0100

    Created a store using appReducer and initialState. Used the store's getState and dispatch functions to mutate the state and log the changes.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var tslib_1 = require("tslib");
+var redux_1 = require("redux");
 var reducers_1 = tslib_1.__importDefault(require("./store/reducers"));
-var constants_1 = require("./constants");
 var initialState_1 = tslib_1.__importDefault(require("./initialState"));
-var state = initialState_1.default;
-var setGoal = {
-    type: constants_1.SET_GOAL,
-    payload: { days: 2 }
-};
-var addDay = {
+var constants_1 = require("./constants");
+var store = redux_1.createStore(reducers_1.default, initialState_1.default);
+console.log('initial state', store.getState());
+store.dispatch({
     type: constants_1.ADD_DAY,
     payload: {
         resort: "Ananke",
-        date: "2020-02-02",
-        powder: true,
+        date: "2013/12/7",
+        powder: false,
         backcountry: true
     }
-};
-var newState = reducers_1.default(reducers_1.default(state, setGoal), addDay);
-console.log("\n    initial : " + JSON.stringify(state) + "\n    actions: " + JSON.stringify(setGoal) + " /n " + JSON.stringify(addDay) + "\n    new goal: " + JSON.stringify(newState) + "\n");
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFDQSxzRUFBd0Q7QUFDeEQseUNBQTBEO0FBRTFELHdFQUF5QztBQUd6QyxJQUFNLEtBQUssR0FBYyxzQkFBWSxDQUFBO0FBRXJDLElBQU0sT0FBTyxHQUFnQjtJQUN6QixJQUFJLEVBQUUsb0JBQVE7SUFDZCxPQUFPLEVBQUUsRUFBRSxJQUFJLEVBQUUsQ0FBQyxFQUFFO0NBQ3ZCLENBQUE7QUFFRCxJQUFNLE1BQU0sR0FBa0I7SUFDMUIsSUFBSSxFQUFFLG1CQUFPO0lBQ2IsT0FBTyxFQUFFO1FBQ0wsTUFBTSxFQUFFLFFBQVE7UUFDaEIsSUFBSSxFQUFFLFlBQVk7UUFDbEIsTUFBTSxFQUFFLElBQUk7UUFDWixXQUFXLEVBQUUsSUFBSTtLQUNwQjtDQUNKLENBQUE7QUFFRCxJQUFNLFFBQVEsR0FBYyxrQkFBVSxDQUFDLGtCQUFVLENBQUMsS0FBSyxFQUFDLE9BQU8sQ0FBQyxFQUFDLE1BQU0sQ0FBQyxDQUFBO0FBRXhFLE9BQU8sQ0FBQyxHQUFHLENBQUMscUJBQ0ssSUFBSSxDQUFDLFNBQVMsQ0FBQyxLQUFLLENBQUMsdUJBQ3RCLElBQUksQ0FBQyxTQUFTLENBQUMsT0FBTyxDQUFDLFlBQVMsSUFBSSxDQUFDLFNBQVMsQ0FBQyxNQUFNLENBQUMsd0JBQ3JELElBQUksQ0FBQyxTQUFTLENBQUMsUUFBUSxDQUFDLE9BQ3hDLENBQUMsQ0FBQSJ9
+});
+console.log('new state', store.getState());
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSwrQkFBaUM7QUFDakMsc0VBQXlDO0FBQ3pDLHdFQUF5QztBQUN6Qyx5Q0FBb0M7QUFFcEMsSUFBTSxLQUFLLEdBQUcsbUJBQVcsQ0FBQyxrQkFBVSxFQUFFLHNCQUFZLENBQUMsQ0FBQztBQUVwRCxPQUFPLENBQUMsR0FBRyxDQUFDLGVBQWUsRUFBRSxLQUFLLENBQUMsUUFBUSxFQUFFLENBQUMsQ0FBQTtBQUU5QyxLQUFLLENBQUMsUUFBUSxDQUFDO0lBQ1gsSUFBSSxFQUFFLG1CQUFPO0lBQ2IsT0FBTyxFQUFFO1FBQ0wsTUFBTSxFQUFFLFFBQVE7UUFDaEIsSUFBSSxFQUFFLFdBQVc7UUFDakIsTUFBTSxFQUFFLEtBQUs7UUFDYixXQUFXLEVBQUUsSUFBSTtLQUNwQjtDQUNKLENBQUMsQ0FBQTtBQUVGLE9BQU8sQ0FBQyxHQUFHLENBQUMsV0FBVyxFQUFFLEtBQUssQ0FBQyxRQUFRLEVBQUUsQ0FBQyxDQUFBIn0=
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,20 @@
-﻿import {GoalAction, SkiDayAction} from "./types/actionTypes";
-import appReducer, {allSkiDays} from "./store/reducers";
-import {ADD_DAY, REMOVE_DAY, SET_GOAL} from "./constants";
-import {AppState, SkiDay} from "./types/stateTypes";
-import initialState from "./initialState"
-import {AnyAction} from "redux";
+﻿import {createStore} from 'redux'
+import appReducer from './store/reducers'
+import initialState from './initialState'
+import {ADD_DAY} from "./constants";
 
-const state : AppState = initialState
+const store = createStore(appReducer, initialState);
 
-const setGoal : GoalAction = {
-    type: SET_GOAL,
-    payload: { days: 2 }
-}
+console.log('initial state', store.getState())
 
-const addDay : SkiDayAction = {
+store.dispatch({
     type: ADD_DAY,
     payload: {
         resort: "Ananke",
-        date: "2020-02-02",
-        powder: true,
+        date: "2013/12/7",
+        powder: false,
         backcountry: true
     }
-}
+})
 
-const newState : AppState = appReducer(appReducer(state,setGoal),addDay)
-
-console.log(`
-    initial : ${ JSON.stringify(state) }
-    actions: ${ JSON.stringify(setGoal) } /n ${ JSON.stringify(addDay) }
-    new goal: ${ JSON.stringify(newState) }
-`)
+console.log('new state', store.getState())
